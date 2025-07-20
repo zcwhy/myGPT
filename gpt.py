81117eaefb3bb4c6146fedda2f1e94c5db9348f3
zcwhy <845562684@qq.com>
2025-07-20 22:52:40 +0800

    GPT架构

---
+++ gpt.py
@@ -0,0 +1,53 @@
+from attention import batch_size
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50527,
+    "context_length": 1024,
+    "emb_dim": 768,
+    "n_heads":12,
+    "n_layers":12,
+    "drop_rate":0.1,
+    "bias":False,
+}
+
+import torch
+import torch.nn as nn
+
+class DummyGPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"],cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"],cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+            *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shap
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+
+class DummyTransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+    def forward(self, x):  # D
+        return x
+
+class DummyLayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-5):  # F
+        super().__init__()
+
+    def forward(self, x):
+        return x
